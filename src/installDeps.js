aeacecc40c0f6bf3b6688483d4fb09ace1525f73
Andy Edwards <jedwards@fastmail.com>
2021-11-24 13:41:58 -0600

    feat: install with pnpm if pnpm-lock.yaml found

--- src/installDeps.js
+++ src/installDeps.js
@@ -12,8 +12,14 @@ export default async function installDeps({
 }: {
   packageDirectory: string,
 }): Promise<void> {
-  if (await fs.exists(path.join(packageDirectory, 'yarn.lock'))) {
+  const [isYarn, isPnpm] = await Promise.all([
+    fs.exists(path.join(packageDirectory, 'yarn.lock')),
+    fs.exists(path.join(packageDirectory, 'pnpm-lock.yaml')),
+  ])
+  if (isYarn) {
     await spawn('yarn', { cwd: packageDirectory, stdio: 'inherit' })
+  } else if (isPnpm) {
+    await spawn('pnpm', ['i'], { cwd: packageDirectory, stdio: 'inherit' })
   } else {
     await spawn('npm', ['install'], {
       cwd: packageDirectory,
